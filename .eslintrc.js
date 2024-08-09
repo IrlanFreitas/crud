35e8cbf52d62799444d8dd95df055bfee960b9d8
Irlan Freitas <irlan.freitas@hotmail.com>
2024-08-09 19:07:14 -0300

    fix: errors lint vercel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,45 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    "cypress/globals": true,
-  },
-  plugins: [
-    "react",
-    "prettier",
-    "@typescript-eslint",
-    "cypress",
-    "chai-friendly",
-  ],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:chai-friendly/recommended",
-    "plugin:cypress/recommended",
-    "plugin:prettier/recommended",
-  ],
-  overrides: [],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  settings: {
-    react: {
-      version: "detect",
+    parser: "@typescript-eslint/parser",
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+        "cypress/globals": true,
     },
-  },
-  ignorePatterns: ["node_modules/"],
-  // Cherry of the Cake
-  rules: {
-    "no-console": ["error", { allow: ["warm", "error"] }],
-    "react/no-unknown-property": ["error", { ignore: ["jsx", "global"] }],
-  },
+    plugins: [
+        "react",
+        "prettier",
+        "@typescript-eslint",
+        "cypress",
+        "chai-friendly",
+    ],
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/recommended",
+        "plugin:chai-friendly/recommended",
+        "plugin:cypress/recommended",
+        "plugin:prettier/recommended",
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+    ignorePatterns: ["node_modules/"],
+    // Cherry of the Cake
+    rules: {
+        "no-console": "warm",
+        "typescript-eslint/no-var-requires": "warm",
+        "typescript-eslint/no-empty-function": "warm",
+        "typescript-eslint/no-unused-vars": "warm",
+        "cypress/no-unnecessary-waiting": "warm",
+        "no-console": ["error", { allow: ["warm", "error"] }],
+        "react/no-unknown-property": ["error", { ignore: ["jsx", "global"] }],
+    },
 };
